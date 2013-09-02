0314b55d21009a3932579d7982b817530bd374d4
Iván López <lopez.ivan@gmail.com>
2013-09-02 17:00:21 +0200

    Add author, documentation,... information

--- MyMalcomApiGrailsPlugin.groovy
+++ MyMalcomApiGrailsPlugin.groovy
@@ -16,37 +16,29 @@ class MyMalcomApiGrailsPlugin {
             "web-app/**"
     ]
 
-    // TODO Fill in these fields
-    def title = "Grails My Malcom Api Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "Grails My Malcom Api Plugin"
+    def author = "Iván López"
+    def authorEmail = "ivan.lopez@kaleidos.net"
     def description = '''\
-Brief summary/description of the plugin.
+This plugin provides a service to send push notifications to your mobile devices via MyMalcom (http://www.mymalcom.com) from your grails application.
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/my-malcom-api"
+    def documentation = "https://github.com/kaleidos/grails-my-malcom/blob/master/README.md"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def organization = [ name: "Kaleidos", url: "http://kaleidos.net/" ]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/kaleidos/grails-my-malcom/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
+    def scm = [ url: "https://github.com/kaleidos/grails-my-malcom" ]
 
     def doWithSpring = {
         def myMalcomApiConfig = application.config.myMalcom
@@ -67,28 +59,5 @@ Brief summary/description of the plugin.
         connectionClient(net.kaleidos.mymalcom.ConnectionClient) {
             grailsApplication = ref("grailsApplication")
         }
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
